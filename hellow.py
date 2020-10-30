705246890105e8ae2e06a71dbc869e85db6bb168
Abhishekthakur29 <55458371+Abhishekthakur29@users.noreply.github.com>
2020-10-30 20:08:25 +0530

    Factorial Of Number

--- hellow.py
+++ hellow.py
@@ -1,1 +1,11 @@
 print("Hello World!")
+# Finding the factorial using the recursion
+def fact(num):
+  if num==1:
+    return 1
+  else:
+    return num* fact(num-1)
+  
+ num = int(input())
+res = fact(num)
+print(res)
